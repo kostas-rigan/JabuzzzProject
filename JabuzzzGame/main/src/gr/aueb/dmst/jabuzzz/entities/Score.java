06347806cf37554c48c74af05d338b68f4504482
kostadis10 <t8200167@aueb.gr>
2021-11-19 18:03:44 +0200

    changes on class Score

--- JabuzzzGame/main/src/gr/aueb/dmst/jabuzzz/entities/Score.java
+++ JabuzzzGame/main/src/gr/aueb/dmst/jabuzzz/entities/Score.java
@@ -1,16 +1,26 @@
 package gr.aueb.dmst.jabuzzz.entities;
 
+import gr.aueb.dmst.jabuzzz.utilities.Exit;
+
 public class Score {
 	int teamsScore;
+	int target;
 	
-	public Score() {
+	public Score(int target) {
 		teamsScore = 0;
+		this.target = target;
 	}
 	public void correctAnswer () {
 		teamsScore += 1;
+		if (teamsScore == target) {
+			Exit.exitGame();
+		}
 	}
 	public void wrongAnswer () {
 		teamsScore -= 1;
+		if (teamsScore == -5) {
+			Exit.exitGame();
+		}
 	}
 	public int getTeamsScore() {
 		return teamsScore;
