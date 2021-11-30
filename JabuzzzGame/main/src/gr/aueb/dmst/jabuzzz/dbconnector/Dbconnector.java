698beea5d529aa61e58ce6771f6ff86eddf7ae03
Konstantina Mavroeidi <t8200103@aueb.gr>
2021-11-30 18:45:51 +0200

    created db

---
+++ JabuzzzGame/main/src/gr/aueb/dmst/jabuzzz/dbconnector/Dbconnector.java
@@ -0,0 +1,24 @@
+package gr.aueb.dmst.jabuzzz.dbconnector;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Dbconnector {
+
+		private Connection connect() {
+			
+			String url = "jdbc:sqlite:src/resources/DATA_BASE.sqlite";
+			Connection conn = null;
+			try {
+				conn = DriverManager.getConnection(url);
+				System.out.println("connected");
+			}catch (SQLException e){
+				System.out.println(e.getMessage());
+			}
+			return conn;
+		}
+}
+
---
+++ JabuzzzGame/main/src/gr/aueb/dmst/jabuzzz/dbconnector/Dbconnectortest.java
@@ -0,0 +1,9 @@
+package gr.aueb.dmst.jabuzzz.dbconnector;
+
+public class Dbconnectortest {
+	public static void main (String args) {
+		
+		Dbconnector testing = new Dbconnector();
+		
+	}
+}
