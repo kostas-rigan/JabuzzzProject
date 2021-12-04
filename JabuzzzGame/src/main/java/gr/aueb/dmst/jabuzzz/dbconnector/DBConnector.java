bf5489258d5c8710b188298c6dadaf9f07ea7345
kostas-rigan <t8200145@aueb.gr>
2021-12-04 15:02:41 +0200

    Hopefully fixed Intellij problem

--- JabuzzzGame/src/main/java/gr/aueb/dmst/jabuzzz/dbconnector/DBConnector.java
+++ JabuzzzGame/src/main/java/gr/aueb/dmst/jabuzzz/dbconnector/DBConnector.java
@@ -7,17 +7,18 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DBConnector {
-		private Connection conn;
-		public void connect() {
-			
-			String url = "jdbc:sqlite:src/main/resources/DATA_BASE.sqlite";
-			
-			try {
-				conn = DriverManager.getConnection(url);
-				System.out.println("connected");
-			}catch (SQLException e){
-				System.out.println(e.getMessage());
-			}
-			
-		}
+    private Connection conn;
+
+    public void connect() {
+
+        String url = "jdbc:sqlite:src/main/resources/DATA_BASE.sqlite";
+
+        try {
+            conn = DriverManager.getConnection(url);
+            System.out.println("connected");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
 }
